7df1eb4a33da55dbfa63da74c24186d37b984ef1
scottbisaillon <scott.bisaillon.dev@gmail.com>
2022-04-19 19:23:21 -0400

    chore: test simple-git tags function

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import path from 'path'
+import simpleGit from 'simple-git'
+
+const baseDir = path.join(process.cwd())
+const git = simpleGit({baseDir})
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+  git.tags((err, tags) => {
+    core.debug(tags.all.join(', '))
+  })
 }
 
 run()
